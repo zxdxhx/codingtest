07d21a19155a5bdd05b8085803119fff27c9d7a4
kimdohee <117574267+zxdxhx@users.noreply.github.com>
2023-11-11 21:35:26 +0900

    Update 덩치.java
    변경

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,46 +1,39 @@
-
 import java.util.*;
 public class Main {
-    static int N;
-    static int[] rank;
-    static People[] people;
-
     static class People{
-        int weight;
-        int height;
+        int w;
+        int h;
         People(int w, int h){
-            this.weight = w;
-            this.height = h;
+            this.w = w;
+            this.h = h;
         }
-    }
-
-    static void compare(int index){
-        int count = 1;
-        for(int i = 0; i<N;i++){
-            if(index == i) continue;
-
-            if(people[index].weight<people[i].weight &&people[index].height<people[i].height)
-                count ++;
-        }
-        rank[index] = count;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int[] rank = new int[num];
+        People[] people = new People[num];
 
-        N = scan.nextInt();
-        rank = new int[N];
-        people = new People[N];
-
-        for(int i =0; i<N; i++){
+        for(int i  = 0; i< num; i++) {
             int w = scan.nextInt();
             int h = scan.nextInt();
-            people[i] = new People(w,h);
+            people[i] = new People(w, h);
         }
-
-        for(int i  = 0; i<N; i++){
-            compare(i);
-            System.out.print(rank[i]+" ");
+        for(int i = 0; i< num; i++){
+            int count = 0;
+            for(int j = 0; j < num; j++){
+                People target = new People(people[i].w, people[i].h);
+                if(i == j) continue;
+                if(target.w < people[j].w){
+                    if(target.h < people[j].h){
+                        count++;
+                    }
+                }
+            }
+            rank[i] = count + 1;
         }
-
+        for(int i = 0; i < num; i++){
+            System.out.print(rank[i] + " ");
+        }
     }
 }
